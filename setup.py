2c0ed8956ef8874207524250c68bbc7a2333e56b
0dminnimda <0dminnimda@gmail.com>
2022-07-30 03:59:48 +0300

    setup.py: add the extras_require

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
+from pathlib import Path
+
+
 setup(
     name='pycom.py',
     version='1.0.0',
+    extras_require={
+        "test": Path("test-requirements.txt").read_text(),
+    },
     entry_points={
         'console_scripts': [
             'pycom=src.pycom.pycom:main'
