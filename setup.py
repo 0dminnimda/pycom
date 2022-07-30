6bd9bf62bda982ac5f111e90866bbe61a4daf925
0dminnimda <0dminnimda@gmail.com>
2022-07-30 04:06:24 +0300

    setup.py: install requirements automatically

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+
 from setuptools import setup
+
+
 setup(
     name='pycom.py',
     version='1.0.0',
+    install_requires=Path('requirements.txt').read_text().split("\n"),
     entry_points={
         'console_scripts': [
             'pycom=src.pycom.pycom:main'
