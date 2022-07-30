49e3ff045c2b0eeb55460851d49292fd81552f53
0dminnimda <0dminnimda@gmail.com>
2022-07-30 04:11:48 +0300

    setup.py: fix the formatting

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name='pycom.py',
     version='1.0.0',
-    install_requires=Path('requirements.txt').read_text().split("\n"),
+    install_requires=Path('requirements.txt').read_text().split('\n'),
     entry_points={
         'console_scripts': [
             'pycom=src.pycom.pycom:main'
