cc0d95c4e7d82125a575fd2c5006df075875dc8a
0dminnimda <0dminnimda@gmail.com>
2022-07-30 04:10:49 +0300

    setup.py: fix the formatting

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='pycom.py',
     version='1.0.0',
     extras_require={
-        "test": Path('test-requirements.txt').read_text().split("\n"),
+        'test': Path('test-requirements.txt').read_text().split('\n'),
     },
     entry_points={
         'console_scripts': [
